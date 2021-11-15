4a3bffaed35311fcb3da04df7526c4fc3eb5bd84
CameronBrenton <Cameron.Brenton@keyin.com>
2021-11-15 11:12:45 -0330

    First Commit

---
+++ src/main/java/com/sprint1/Nov15restservice/Nov15RestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sprint1.Nov15restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nov15RestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nov15RestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sprint1/Nov15restservice/Nov15RestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint1.Nov15restservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nov15RestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
