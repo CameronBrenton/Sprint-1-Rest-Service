8e4a8970901d34c9918ed7ca9202b464bc2d2669
CameronBrenton <Cameron.Brenton@keyin.com>
2021-11-19 13:25:55 -0330

    First commit

---
+++ src/main/java/com/sprint1/Nov15restservice/accessingdatarest/repository/PastTournamentRepository.java
@@ -0,0 +1,16 @@
+package com.sprint1.Nov15restservice.accessingdatarest.repository;
+
+
+import com.sprint1.Nov15restservice.accessingdatarest.model.PastTournament;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+
+@RepositoryRestResource(collectionResourceRel = "pastTournaments", path = "pastTournaments")
+public interface PastTournamentRepository extends PagingAndSortingRepository<PastTournament, Long> {
+    List<PastTournament> findByPastTournamentDate(@Param("pastTournamentDate") String pastTournamentDate);
+}
+
